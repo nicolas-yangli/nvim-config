cd3ed465fbec1717ce280e7c3f8de5130e84fa5e
Nicholas Yang <bill.lee.y@gmail.com>
2024-02-24 13:05:21 +0800

    initial commit

---
+++ init.lua
@@ -0,0 +1,113 @@
+vim.opt.fileformats = {'unix', 'dos'} 
+
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.diffopt:append('vertical')
+
+vim.api.nvim_create_autocmd(
+  'FileType',
+  {
+    pattern = {'python'},
+    callback = function()
+      vim.opt.colorcolumn = '120'
+      vim.opt.textwidth = 118
+    end
+  }
+)
+vim.api.nvim_create_autocmd(
+  'FileType',
+  {
+    pattern = {'css,json,yaml,lua'},
+    callback = function()
+      vim.opt.shiftwidth = 2
+      vim.opt.tabstop = 2
+    end
+  }
+)
+
+vim.keymap.set('t', '<A-A>', ':q')
+vim.keymap.set('t', '<A-C>', '<C-\\><C-n>')
+vim.keymap.set('n', '<Leader>ts', ':vsplit term://$SHELL<Enter>')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  {
+    'tanvirtin/monokai.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function() vim.cmd('colorscheme monokai') end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup({
+        options = {
+          section_separators = '',
+          component_separators = '',
+        },
+        tabline = {
+          lualine_a = {'filename'},
+          lualine_b = {'buffers'},
+          lualine_c = {'branch'},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {'tabs'},
+        }
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    ft = { 'lua', 'vim', 'python', 'toml' },
+    config = function () 
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "vim", "python", 'toml' },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },  
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require('mylspconfig').setup()
+    end,
+    ft = { 'python', 'sh' },
+  },
+  {
+    "nicolas-yangli/nvim-git-blame",
+    build = ":UpdateRemotePlugins",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    ft = { 'python', },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function ()
+      require('mycmpconfig').setup()
+    end,
+  },
+  {
+    "mmarchini/bpftrace.vim",
+    event = { 'BufEnter *.bt' },
+  }
+})
---
+++ lua/mylspconfig.lua
@@ -0,0 +1,43 @@
+M = {}
+
+function M.setup()
+  lspconfig = require('lspconfig')
+
+  lspconfig.pyright.setup({})
+  lspconfig.bashls.setup({
+    cmd = {"podman", "run", "--rm", "-i", "--workdir", vim.loop.cwd(), "--pid", "host", "-v", "/home:/home:ro", "nicolasyang.me/bash-language-server"}
+  })
+
+  -- Use LspAttach autocommand to only map the following keys
+  -- after the language server attaches to the current buffer
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+      -- Enable completion triggered by <c-x><c-o>
+      vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+      -- Buffer local mappings.
+      -- See `:help vim.lsp.*` for documentation on any of the below functions
+      local opts = { buffer = ev.buf }
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', '<C-q>', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+      --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+      --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+      --vim.keymap.set('n', '<space>wl', function()
+      --  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      --end, opts)
+      vim.keymap.set('n', '<Leader>td', vim.lsp.buf.type_definition, opts)
+      vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      vim.keymap.set('n', '<Leader>f', function()
+        vim.lsp.buf.format { async = true }
+      end, opts)
+    end
+  })
+end
+
+return M
