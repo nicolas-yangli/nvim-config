1dd0b6e098fcc4c2e7c25b39b1268fca72b94d72
Nicholas Yang <bill.lee.y@gmail.com>
2025-07-02 14:38:33 +0800

    chore(mylspconfig): adapt to neovim global keymap new defaults

--- lua/mylspconfig.lua
+++ lua/mylspconfig.lua
@@ -1,4 +1,4 @@
-M = {}
+local M = {}
 
 function M.setup()
   lspconfig = require('lspconfig')
@@ -17,24 +17,26 @@ function M.setup()
       -- Buffer local mappings.
       -- See `:help vim.lsp.*` for documentation on any of the below functions
       local opts = { buffer = ev.buf }
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'grD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'grd', vim.lsp.buf.definition, opts)
       vim.keymap.set('n', '<C-q>', vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
       --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
       --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
       --vim.keymap.set('n', '<space>wl', function()
       --  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
       --end, opts)
-      vim.keymap.set('n', '<Leader>td', vim.lsp.buf.type_definition, opts)
-      vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
-      vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, opts)
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
       vim.keymap.set('n', '<Leader>f', function()
         vim.lsp.buf.format { async = true }
       end, opts)
       vim.keymap.set('n', '<Leader>ld', vim.diagnostic.open_float, opts)
+      -- vim.keymap.del('i', vim.lsp.buf.signature_help)
+      vim.keymap.set({ 'n', 'v', 'i' }, '<C-k>', vim.lsp.buf.signature_help, opts)
+      -- default keymaps
+      -- n  grn:    vim.lsp.rename
+      -- n  gri:    vim.lsp.buf.implementation
+      -- n  gO:     vim.lsp.buf.document_symbol
+      -- n  grr:    vim.lsp.buf.references
+      -- nv gra:    vim.lsp.buf.code_action
     end
   })
 end
